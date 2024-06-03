0d1cabd0f2ee4be8a842416b2755bb260406acc1
Karolfm12 <karolfm12@onet.eu>
2024-06-03 13:35:20 +0000

    Deploying to gh-pages from @ Karolfm12/goit-react-hw-08-phonebook@40ece9992f1170129a9f19ebf62535c736f109dd 🚀

---
+++ static/js/282.38d77115.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[282],{6282:function(e,t,r){r.r(t),r.d(t,{default:function(){return f}});var n=r(2791),c=r(4270),s=r(4420),u=r(1417),a=r(184);function f(){var e=(0,s.I0)();return(0,n.useEffect)((function(){e((0,u.yF)())}),[e]),(0,a.jsxs)(a.Fragment,{children:[(0,a.jsx)(c.q,{children:(0,a.jsx)("title",{children:"Your tasks"})}),(0,a.jsx)(f,{})]})}}}]);
+//# sourceMappingURL=282.38d77115.chunk.js.map
